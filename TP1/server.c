53386158bd81ad91c8b474ad630db98e9cd23542
Cecília Nascimento <cecilia.pereira@dcc.ufmg.br>
2021-11-15 20:28:30 -0300

    Initial configuration of server side

---
+++ TP1/server.c
@@ -0,0 +1,33 @@
+#include "common.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
+
+#define BUFSZ 500
+
+void usage(int argc, char **argv) {
+    printf("usage: %s <v4|v6> <server port>\n", argv[0]);
+    printf("example: %s v4 51511\n", argv[0]);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        usage(argc, argv);
+    }
+
+    struct sockaddr_storage storage;
+    if (0 != server_sockaddr_init(argv[1], argv[2], &storage)) {
+        usage(argc, argv);
+    }
+
+    int s;
+    s = socket(storage.ss_family, SOCK_STREAM, 0);
+    if (s == -1) {
+        logexit("socket");
+    }
+
+    exit(EXIT_SUCCESS);
+}
