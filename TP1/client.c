f18b4f09331a04314fd293527da97cf3e38e1042
Cecília Nascimento <cecilia.pereira@dcc.ufmg.br>
2021-11-15 20:26:32 -0300

    add commons functions and client side

--- TP1/client.c
+++ TP1/client.c
@@ -1,7 +1,13 @@
-#include <stdlib.h>
+#include "common.h"
 
-#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
 
 #define BUFSZ 500  
 
@@ -13,22 +19,54 @@ void usage(int argc, char **argv) {
 
 int main(int argc, char const* argv[]) {
     
-    struct sockaddr_storage storage;
-
     if (argc < 3) {
 		usage(argc, argv);
 	}
+    struct sockaddr_storage storage;
+	struct sockaddr *addr = (struct sockaddr *)(&storage);
+	int s;
+
 	if (0 != addrparse(argv[1], argv[2], &storage)) {
 		usage(argc, argv);
 	}
 
-	int s;
 	s = socket(storage.ss_family, SOCK_STREAM, 0);
 	if (s == -1) {
 		logexit("socket failed");
 	}
-	struct sockaddr *addr = (struct sockaddr *)(&storage);
 	if (0 != connect(s, addr, sizeof(storage))) {
 		logexit("connect failed");
 	}
+
+	char addrstr[BUFSZ];
+	addrtostr(addr, addrstr, BUFSZ);
+
+	printf("connected to %s\n", addrstr);
+
+	char buf[BUFSZ];
+	memset(buf, 0, BUFSZ);
+	printf("mensagem> ");
+	fgets(buf, BUFSZ-1, stdin);
+	
+	size_t count = send(s, buf, strlen(buf)+1, 0);
+
+	if (count != strlen(buf)+1) {
+		logexit("send failed");
+	}
+
+	memset(buf, 0, BUFSZ);
+	unsigned total = 0;
+	while(1) {
+		count = recv(s, buf + total, BUFSZ - total, 0);
+		if (count == 0) {
+			break;
+		}
+		total += count;
+	}
+	close(s);
+
+	printf("received %u bytes\n", total);
+	puts(buf);
+
+	exit(EXIT_SUCCESS);
 }
